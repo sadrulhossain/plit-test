01c985df9d2b6a8b5f6e99e0d859ff5411debfcb
Sadrul <hossainsadrul@gmail.com>
2023-01-22 23:49:11 +0600

    user panel done

--- plit_db.sql
+++ plit_db.sql
@@ -1,36 +1,35 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
+-- Server version:               10.4.17-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.3.0.6589
+-- HeidiSQL Version:             11.2.0.6213
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Dumping database structure for plit_test
+DROP DATABASE IF EXISTS `plit_test`;
 CREATE DATABASE IF NOT EXISTS `plit_test` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `plit_test`;
 
 -- Dumping structure for table plit_test.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
   `group_id` int(12) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
-  `status` enum('1','2') DEFAULT '1' COMMENT '1=Active, 2=Inactive',
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` enum('1','2') COLLATE utf8_unicode_ci DEFAULT '1' COMMENT '1=Active, 2=Inactive',
   `created_at` datetime DEFAULT NULL,
   `created_by` int(12) DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
@@ -40,23 +39,25 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Dumping data for table plit_test.users: ~0 rows (approximately)
 DELETE FROM `users`;
-INSERT INTO `users` (`id`, `group_id`, `name`, `email`, `phone`, `username`, `password`, `remember_token`, `status`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
-	(1, 1, 'Sadrul Hossain', 'hossainsadrul@gmail.com', '+8801885547800', 'admin', NULL, NULL, '1', '2023-01-22 16:58:20', 1, '2023-01-22 16:58:28', 1);
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `group_id`, `name`, `email`, `phone`, `photo`, `username`, `password`, `remember_token`, `status`, `created_at`, `created_by`, `updated_at`, `updated_by`) VALUES
+	(1, 1, 'Sadrul Hossain', 'hossainsadrul@gmail.com', '+8801885547800', '63cd762d25d6d_1.PNG', 'admin', '$2y$10$REOAlERxTnbVmHytUw6qOOHv7OU.GYB4ur.lUuvhGMgCvPWiAmnQC', NULL, '1', '2023-01-22 16:58:20', 1, '2023-01-22 17:45:17', 1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumping structure for table plit_test.user_group
-DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE IF NOT EXISTS `user_group` (
   `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table plit_test.user_group: ~0 rows (approximately)
+-- Dumping data for table plit_test.user_group: ~1 rows (approximately)
 DELETE FROM `user_group`;
+/*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
 INSERT INTO `user_group` (`id`, `name`) VALUES
 	(1, 'Admin');
+/*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
